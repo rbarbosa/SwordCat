40cba98b8297a7f8e8c73a34f0805714f8657c6b
Rui Barbosa <rui.barbosa@hey.com>
2024-11-21 14:40:47 +0000

    Add TestApp to run tests
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

--- SwordCat/AppFeature/SwordCatApp.swift
+++ SwordCat/AppFeature/SwordCatApp.swift
@@ -7,11 +7,31 @@
 
 import SwiftUI
 
-@main
 struct SwordCatApp: App {
     var body: some Scene {
         WindowGroup {
             AppView()
+        }
+    }
+}
+
+// MARK: - TestView
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Running Unit Tests!")
+        }
+    }
+}
+
+@main
+struct AppLauncher {
+    static func main() {
+        if NSClassFromString("XCTestCase") != nil {
+            TestApp.main()
+        } else {
+            SwordCatApp.main()
         }
     }
 }
