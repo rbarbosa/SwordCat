9a4082865acb2e0a2d7fb43bd378eb17449209b8
Rui Barbosa <rui.barbosa@hey.com>
2024-11-21 14:58:54 +0000

    Add ImageRepositoryMocks
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

---
+++ SwordCat/ImagesRepository/ImagesRepositoryMocks.swift
@@ -0,0 +1,22 @@
+//
+//  ImagesRepositoryMocks.swift
+//  SwordCat
+//
+//  Created by Rui Barbosa on 21/11/2024.
+//
+
+import Foundation
+
+// MARK: - Mocks
+
+#if DEBUG
+extension ImagesRepository {
+    static var success: Self {
+        .init(
+            fetchImages: {
+                .init(breeds: [.mock])
+            }
+        )
+    }
+}
+#endif
