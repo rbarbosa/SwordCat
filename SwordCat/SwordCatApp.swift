c87c592d480920cb091416521d11ded263676ddd
Rui Barbosa <rui.barbosa@hey.com>
2024-11-21 09:56:57 +0000

    Initial Commit

---
+++ SwordCat/SwordCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwordCatApp.swift
+//  SwordCat
+//
+//  Created by Rui Barbosa on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwordCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwordCatTests/SwordCatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwordCatTests.swift
+//  SwordCatTests
+//
+//  Created by Rui Barbosa on 21/11/2024.
+//
+
+import Testing
+@testable import SwordCat
+
+struct SwordCatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
