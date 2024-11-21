bac155366b958bc78e78109b9becc6be97ab68ed
Rui Barbosa <rui.barbosa@hey.com>
2024-11-21 14:34:20 +0000

    Add ImagesRepository
    This repository will feed the breed images.
    
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

---
+++ SwordCat/ImagesRepository/ImagesRepository.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesRepository.swift
+//  SwordCat
+//
+//  Created by Rui Barbosa on 21/11/2024.
+//
+
+import Foundation
+
+struct ImagesRepository {
+    var fetchImages: () async throws -> ImagesRepositoryResponse
+}
+
+
+struct ImagesRepositoryResponse {
+    let breeds: [Breed]
+}
+
