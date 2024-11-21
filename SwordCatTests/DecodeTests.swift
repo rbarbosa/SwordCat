07777a12505f3aa0c7c63c8561d4b9c69149b6a6
Rui Barbosa <rui.barbosa@hey.com>
2024-11-21 17:33:43 +0000

    Add breeds decoding test
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

--- SwordCatTests/DecodeTests.swift
+++ SwordCatTests/DecodeTests.swift
@@ -79,10 +79,74 @@ struct DecodeTests {
         #expect(breed.lifeSpan == "10 - 15")
         #expect(breed.temperament == "Agile, Easy Going, Intelligent, Playful")
         #expect(breed.url == URL(string: "https://cdn2.thecatapi.com/images/a1g4Ycw-z.jpg")!)
-
-
     }
 
+    @Test("Decoding fetch breeds", .tags(.json))
+    func decodeFetchBreed() throws {
+        let breedsJSON: Data = """
+        [
+            {
+                "weight": {
+                    "imperial": "7  -  10",
+                    "metric": "3 - 5"
+                },
+                "id": "abys",
+                "name": "Abyssinian",
+                "cfa_url": "http://cfa.org/Breeds/BreedsAB/Abyssinian.aspx",
+                "vetstreet_url": "http://www.vetstreet.com/cats/abyssinian",
+                "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/abyssinian",
+                "temperament": "Active, Energetic, Independent, Intelligent, Gentle",
+                "origin": "Egypt",
+                "country_codes": "EG",
+                "country_code": "EG",
+                "description": "The Abyssinian is easy to care for, and a joy to have in your home.",
+                "life_span": "14 - 15",
+                "indoor": 0,
+                "lap": 1,
+                "alt_names": "",
+                "adaptability": 5,
+                "affection_level": 5,
+                "child_friendly": 3,
+                "dog_friendly": 4,
+                "energy_level": 5,
+                "grooming": 1,
+                "health_issues": 2,
+                "intelligence": 5,
+                "shedding_level": 2,
+                "social_needs": 5,
+                "stranger_friendly": 5,
+                "vocalisation": 1,
+                "experimental": 0,
+                "hairless": 0,
+                "natural": 1,
+                "rare": 0,
+                "rex": 0,
+                "suppressed_tail": 0,
+                "short_legs": 0,
+                "wikipedia_url": "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+                "hypoallergenic": 0,
+                "reference_image_id": "0XYvRd7oD",
+                "image": {
+                    "id": "0XYvRd7oD",
+                    "width": 1204,
+                    "height": 1445,
+                    "url": "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"
+                }
+            }
+        ]
+        """.data(using: .utf8)!
 
+        let breedsRepositoryResponse = try makeBreedsRepositoryResponse(from: breedsJSON)
 
+        #expect(breedsRepositoryResponse.breeds.count == 1)
+
+        let breed = try #require(breedsRepositoryResponse.breeds.first)
+
+        #expect(breed.id == "abys")
+        #expect(breed.name == "Abyssinian")
+        #expect(breed.description == "The Abyssinian is easy to care for, and a joy to have in your home.")
+        #expect(breed.lifeSpan == "14 - 15")
+        #expect(breed.temperament == "Active, Energetic, Independent, Intelligent, Gentle")
+        #expect(breed.url == URL(string: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg")!)
+    }
 }
