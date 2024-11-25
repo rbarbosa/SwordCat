7eb4182869a212d6cb7421d53d5e99dd0937c679
Rui Barbosa <rui.barbosa@hey.com>
2024-11-25 09:02:48 +0000

    Add fetch image ids to FavoritesManager
    This allows to get the image ids faster without having to do an after request to each image id.
    
    Signed-off-by: Rui Barbosa <rui.barbosa@hey.com>

--- SwordCat/FavoritesManager/FavoritesManager.swift
+++ SwordCat/FavoritesManager/FavoritesManager.swift
@@ -19,6 +19,9 @@ actor FavoritesManager {
     var favoritesImage: IdentifiedArrayOf<FavoriteImage> = []
     var favoriteBreeds: IdentifiedArrayOf<Breed> = []
     var favoriteBreedIds: [String: Int] = [:]
+    
+    /// Handy dictionary where the key is the `imageId` and the value is favorite `id`
+    var favoriteImageIds: [String: Int] = [:]
 
     let repository: FavoritesRepository
     let user: User
@@ -55,32 +58,61 @@ actor FavoritesManager {
         return favoriteBreeds
     }
 
+    func fetchFavoriteImageIds() async throws -> IdentifiedArrayOf<FavoriteImage> {
+        let response = try await repository.fetchFavorites(user.id)
+        var newFavorites: IdentifiedArrayOf<FavoriteImage> = []
+
+        if favoriteBreeds.isEmpty {
+            newFavorites = .init(uniqueElements: response.favoriteImages)
+            favoritesImage = newFavorites
+        } else {
+            let filteredFavorites = response.favoriteImages.filter { newFavoriteImage in
+                !favoritesImage.contains(where: { $0.imageId == newFavoriteImage.imageId })
+            }
+            newFavorites = .init(uniqueElements: filteredFavorites)
+            favoritesImage.append(contentsOf: filteredFavorites)
+        }
+
+        for favorite in newFavorites {
+            favoriteImageIds[favorite.imageId] = favorite.id
+        }
+        return favoritesImage
+    }
+
     func addFavorite(_ breed: Breed) async -> Bool {
+        if let _ = favoriteImageIds[breed.referenceImageId] {
+            // It's already marked as favorite
+            return true
+        }
+
         do {
             let response = try await repository.markAsFavorite(user.id, breed.referenceImageId)
             favoriteBreedIds[breed.id] = response.id
             favoriteBreeds[id: breed.id] = breed
             let favImage = FavoriteImage(id: response.id, imageId: breed.referenceImageId)
             favoritesImage[id: favImage.id] = favImage
+            favoriteImageIds[breed.referenceImageId] = response.id
 
             return true
         } catch {
+            print("Failed to added favorite - \(breed.name) with id: \(breed.referenceImageId)")
             return false
         }
     }
 
     func removeFavorite(_ breed: Breed) async -> Bool {
-        guard let favoriteId = favoriteBreedIds[breed.id] else { return false }
+        guard let favoriteId = favoriteImageIds[breed.referenceImageId] else { return false }
 
         let response = await repository.markAsUnfavorite(favoriteId)
         if response.success {
             favoriteBreedIds[breed.id] = nil
             favoriteBreeds.remove(id: breed.id)
             favoritesImage.remove(id: favoriteId)
+            favoriteImageIds[breed.referenceImageId] = nil
 
             return true
         }
-        
+
         return false
     }
 }
